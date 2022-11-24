bd30e6b33a949a01878363fa7775ce0a9db34e2c
ivelov <ivelov.vlad1@gmail.com>
2022-11-24 18:11:02 +0200

    refactor: delete isAuth.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-if($_POST['login'] === 'admin' && $_POST['email'] === 'admin@gmail.com' && $_POST['password'] === '123'){
+if($_POST['login'] === 'admin'  && $_POST['password'] === '123'){
     echo true;
 }else{
     echo false;
